13593b0436ec20e403fa9aeb394c1f0cb35bbe63
Vasco Tenner <vasco@tenner.nl>
2016-01-18 14:20:58 +0100

    Changed if/else to working case statement

--- set_background.sh
+++ set_background.sh
@@ -3,13 +3,16 @@
 #Set $1 as background
 WINDOWMANAGER=GNOME3
 
-if [ $WINDOWMANAGER=='GNOME' ]; then
+case "$WINDOWMANAGER" in
+"GNOME")
     # See /home/tenner/src/SyncWall-2.0.0-src/src/SystemIntegration.cpp
     gsettings set org.gnome.desktop.background draw-background true
     gsettings set org.gnome.desktop.background picture-options spanned
     gsettings set org.gnome.desktop.background picture-uri "$1"
-elif [ $WINDOWMANAGER=='GNOME3' ]; then
+    ;;
+"GNOME3")
     gsettings set org.mate.background draw-background true
     gsettings set org.mate.background picture-options spanned
     gsettings set org.mate.background picture-filename "$1"
-fi
+    ;;
+esac
