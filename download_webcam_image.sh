5ee561a28ee91e3f3d38128a8de0f4a1ab36fd3b
Dirk Boonzajer Flaes <dbs660@vu.nl>
2016-01-21 12:38:47 +0100

    Autodetect desktop environment

--- download_webcam_image.sh
+++ download_webcam_image.sh
@@ -5,25 +5,24 @@
 # can be used in conjunction with mate and CRON to update the desktop background every N minutes.
 
 function set_background() {
-	case "$WINDOWMANAGER" in
-	"GNOME")
+	case "$DESKTOP_SESSION" in
+	"gnome")
 	    # See /home/tenner/src/SyncWall-2.0.0-src/src/SystemIntegration.cpp
 	    gsettings set org.gnome.desktop.background draw-background true
 	    gsettings set org.gnome.desktop.background picture-options spanned
 	    gsettings set org.gnome.desktop.background picture-uri "file://$1"
 	    ;;
-	"GNOME3")
+	"gnome3")
 	    gsettings set org.mate.background draw-background true
 	    gsettings set org.mate.background picture-options spanned
 	    gsettings set org.mate.background picture-filename "$1"
 	    ;;
-	"CINNAMON")
+	"cinnamon")
 	    gsettings set org.cinnamon.desktop.background picture-uri "file://$1"
 	    ;;
 	esac
 	}
 
-WINDOWMANAGER=CINNAMON
 STORAGE_FOLDER=~/Pictures/webcamlog/
 PICTURE_PATH=~/Pictures/wallpaper/ahwahnee2.jpg
 # This one is actually more beautiful, but it is broken
