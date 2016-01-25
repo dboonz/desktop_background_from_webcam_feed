87d4fbb4f618dec0c27aaf944f814dd5994d9e15
Vasco Tenner <vasco@tenner.nl>
2016-01-25 11:44:43 +0100

    Added manual and possibility for script arguments

--- repeater.sh
+++ repeater.sh
@@ -1,7 +1,18 @@
 #!/bin/bash 
 
+if [[  $# <  2 ]]; then
+    echo "Usage: $0 timeout script [script arguments]"
+    echo ""
+    echo " - timeout in seconds "
+    echo "To run a certain script every N seconds during a Xsession, put this script in Preferences->Startup applications"
+    exit
+fi
+
 timeout=$1
-script="$2"
+
+#remove timeout from argc
+shift
+script="$@"
 
 $script
 
